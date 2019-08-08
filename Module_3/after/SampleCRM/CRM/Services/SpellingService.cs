5a21f42d28d7dcd4abf7f09b36d32fa9824dbf72
jamesmillar <jmillar@sailr.co.uk>
2019-08-08 15:47:28 +0100

    module 4

---
+++ Module_3/after/SampleCRM/CRM/Services/SpellingService.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+using CRM.ViewModels;
+using Microsoft.Azure.CognitiveServices.Language.SpellCheck;
+
+namespace CRM.Services
+{
+    public class SpellingService
+    {
+        private string London = "Lat: 51.507; Long: 0.127; re:1000;";
+
+        private string Key = "86463f82092a4859a2c5250b6f1e36d0";
+
+        public async Task<string> CheckSpellingAsync(ArticleViewModel model)
+        {
+
+            var market = "en-GB";
+            var clientId = ""; // we don't have this yet
+            var geoLocation = London;
+            var ipAddress = GetPublicIpAddress();
+
+            SpellCheckClient client = new SpellCheckClient(new ApiKeyServiceClientCredentials(Key));
+
+           var response = await
+                client.SpellCheckerWithHttpMessagesAsync(model.Body, market: market,
+                clientId: clientId, clientIp: ipAddress, location: geoLocation);
+
+            return await response.Response.Content.ReadAsStringAsync();
+        }
+
+        private string GetPublicIpAddress()
+        {
+            string publicIP = new System.Net.WebClient().DownloadString("https://api.ipify.org");
+            return publicIP;
+
+        }
+
+    }
+
+}
---
+++ Module_3/after/SampleCRM/CRM/ViewModels/ArticleViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRM.ViewModels
+{
+    public class ArticleViewModel
+    {
+        public int Id { get; set; }
+
+        [DataType(DataType.Text)]
+        [Required]
+        public string Title { get; set; }
+
+       [Required]
+       [DataType(DataType.MultilineText)]
+       [AllowHtml]
+        public string Body { get; set; }
+
+        public string SpellingResponse { get; set; }
+    }
+}
